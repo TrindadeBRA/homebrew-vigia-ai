99137b4a5b81dddfc4de1a907e18d712f8146fb2
Trindade <trindadebra@gmail.com>
2026-09-06 18:21:11 -0300

    cask: vigia-ai 2.8.0

--- Casks/vigia-ai.rb
+++ Casks/vigia-ai.rb
@@ -1,9 +1,9 @@
 cask "vigia-ai" do
   arch arm: "-arm64"
 
-  version "2.7.0"
-  sha256 arm:   "3c9c7905fbf5dcd3d0ef096fc45db31316346092a301649b96666070c9cab447",
-         intel: "dc05a240127629e8dbbcf66a28104a3fc33302f02a1313adbcf69e518d9e4a1d"
+  version "2.8.0"
+  sha256 arm:   "b3c99379e88ed766a4c5ec9d2299548148ba7033b374151b8f95060c4aaf47fa",
+         intel: "54621a27d435e7c22626047951b7adb548a9ef1706aafc8d6e59f1c08fa09c4c"
 
   url "https://github.com/TrindadeBRA/vigia-ai/releases/download/v#{version}/Vigia-AI-#{version}#{arch}.dmg"
   name "Vigia AI"
